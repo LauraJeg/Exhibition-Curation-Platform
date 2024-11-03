4a0e1146abd407edfd8033d4094cbf695a58cee9
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-11-03 18:34:58 +0000

    this api doesnt have enough images in it

--- curator-project/api.js
+++ curator-project/api.js
@@ -41,20 +41,15 @@ export const clev2ApiTest = () => {
 export const testBothAPI = (params) => {
   const clevFormatting = params.split(" ").join("+");
   const clevPromise = clevAPI.get(`?artists=${clevFormatting}&limit=10`);
-  //artist=${clevFormatting}
+
 
   const VAPromise = VAAPI.get(`?q_actor=${params}`);
-  //q_actor=${params}
-
+  
   return Promise.all([clevPromise, VAPromise]).then((results) => {
     const data = {
-      clevData: results[0].data,
+      clevData: results[0].data.data,
       VAData: results[1].data.records,
     };
-    // console.log(results[0].data.data)
-    // console.log(results[1].data.records)
-    console.log(data)
     return data;
   });
 }
-testBothAPI('picasso')
--- curator-project/src/App.jsx
+++ curator-project/src/App.jsx
@@ -1,19 +1,66 @@
 import { useState, useEffect } from 'react'
-import {  } from '../api';
+import { testBothAPI } from '../api';
 
 import './App.css'
 
 function App() {
+  const [results, setResults] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    testBothAPI('picasso').then((res) => {
+      setResults(res);
+      setIsLoading(false);
+    });
   }, []);
+  isLoading===false?console.log(results.clevData[0].images):console.log('no')
 
   return (
     <>
-      <p className="read-the-docs">
-        hello
-      </p>
-    </>
+
+          {isLoading === false? (
+            <>
+            {results.clevData.map((art) => {
+            return (
+              <div key={art.id}>
+                <h2>{art.title}</h2>
+                <img src={art.images?.web?.url} alt={art.title} />
+              </div>
+            );
+          })}
+             
+              </>):
+              <p></p>
+          } 
+      {/* {results.clevData.length > 0 || results.VAData.length? (
+        <>
+          {" "}
+          {results.clevData.map((art) => {
+            return (
+              <div key={art.id}>
+                <h2>{art.title}</h2>
+                <img src={art.images?.web?.url} alt={art.title} />
+              </div>
+            );
+          })} */}
+          {/* {results.artObjects.map((result) => {
+            return (
+              <div key={result.id}>
+                <h2>{result.title}</h2>
+                <img src={result.webImage.url} alt={result.title} />
+              </div>
+            );
+          })} */}
+        </>
+  //     ) : (
+  //       <p>Nothing to display</p>
+  //     )}
+
   )
 }
 
 export default App
+
+{/* <div key={results.clevData[0].id}>
+<h2>{results.clevData[0].title}</h2>
+<img src={results.clevData[0].images?.web?.url} alt={results.clevData[0].title} />
+</div> */}
