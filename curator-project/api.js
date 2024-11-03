3982c1a3f0d17adfcbb09f46d54ef030d391bf37
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-11-03 15:25:05 +0000

    added api functionality for Met Museum

--- curator-project/api.js
+++ curator-project/api.js
@@ -42,15 +42,19 @@ export const metSearchByObjectID = (objectID) => {
     });
 }
 
+export const MetAddDataToObj = (artObjIDArr) => {
+    const artObj = [];
+    artObjIDArr.forEach((objectID)=> {
+      metSearchByObjectID(objectID).then((art)=> artObj.push(art))
+    })
+    return artObj
+}
+
 export const testMetQueryAPI = () => {
   return axios
     .get('https://collectionapi.metmuseum.org/public/collection/v1/search?q=Longacre')
-    .then(({ artObjArr }) => {
-      artObjArr.forEach((art)=> {
-        //for each ID put into function to grab whole obj
-        //then add to a new arr
-      })
-      return data;
+    .then(( {data} ) => {   
+      return MetAddDataToObj(data.objectIDs);
     })
     .catch((error) => {
       console.error('Error fetching artworks:', error);
