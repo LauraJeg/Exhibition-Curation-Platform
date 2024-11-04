5f0264231963e150f11db7c72212aa5ac1dbde70
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-11-04 06:20:04 +0000

    formatted card

--- curator-project/src/Components/common/CommonArtCard.jsx
+++ curator-project/src/Components/common/CommonArtCard.jsx
@@ -9,11 +9,8 @@ import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useState, useEffect } from 'react'
 import { getOneVAArt } from '../../../api/api';
 
@@ -43,9 +40,12 @@ const ExpandMore = styled((props) => {
   
     
 const CommonArtCard = () => {
+
     const [results, setResults] = useState();
     const [isLoading, setIsLoading] = useState(true);
-    React.useEffect(() => {
+
+    //testing with one artwork
+    useEffect(() => {
     getOneVAArt('O1173325').then((res)=> {
             setResults(res.records[0]);
             setIsLoading(false);
@@ -53,46 +53,43 @@ const CommonArtCard = () => {
     }, []);
     isLoading===false?console.log(results):console.log('no')
 
-    const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     const handleExpandClick = () => {
       setExpanded(!expanded);
     };
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <>
+
+        {isLoading === false? (
+        <Card key = {results.systemNumber} 
+            sx={{ maxWidth: 345 , m:4, backgroundcolor:'primary',
+                backgroundColor: '#ebe6e3'}}>
           <CardHeader
             avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
+              <Avatar alt="V&A" src="'../../../assets/V&Asymbol.jpg" aria-label="museumIcon"/>
             }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
+            title={results._primaryTitle}
+            // subheader={`Dated: ${results.accessionNumber}`}
+            subheader={`Made by: ${results._primaryMaker.name}`}
           />
           <CardMedia
             component="img"
             height="194"
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0mHu1hb9RgR8W5MUIh75KwHaFW%26pid%3DApi&f=1&ipt=88f2089cb71b3ac047c71a062ab03dce3d28d0a2c2cc4e92a80941060c45045a&ipo=images"
+            image={results._images._primary_thumbnail}
             alt="Paella dish"
           />
           <CardContent>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
+            <Typography variant="body2" align='right' sx={{ color: 'text.secondary' , margin: 1, fontWeight:'bold'}}>
+            {`Dated: ${results.accessionNumber}`}
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              No description provided for this piece.
             </Typography>
           </CardContent>
           <CardActions disableSpacing>
             <IconButton aria-label="add to favorites">
               <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
             </IconButton>
             <ExpandMore
               expand={expanded}
@@ -105,34 +102,19 @@ const CommonArtCard = () => {
           </CardActions>
           <Collapse in={expanded} timeout="auto" unmountOnExit>
             <CardContent>
-              <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                aside for 10 minutes.
-              </Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                large plate and set aside, leaving chicken and chorizo in the pan. Add
-                pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                stirring often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Add rice and stir very gently to distribute. Top with artichokes and
-                peppers, and cook without stirring, until most of the liquid is absorbed,
-                15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                mussels, tucking them down into the rice, and cook again without
-                stirring, until mussels have opened and rice is just tender, 5 to 7
-                minutes more. (Discard any mussels that don&apos;t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              <Typography sx={{ marginBottom: 2, color: 'text.secondary'  }}>
+                 {`Type: ${results.objectType}`}
+            </Typography>
+              <Typography sx={{ marginBottom: 2 , color: 'text.secondary' }}>
+                {results._currentLocation.onDisplay? 'This piece is displayed in the V&A now':'This piece is in storage'}
               </Typography>
             </CardContent>
           </Collapse>
         </Card>
+        ):
+        <p>nothing</p>
+        }
+        </>
       );
 };
 
