5f374c02dd464b0eb24f6875ade1bf0e3c7b4fd2
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-11-04 22:51:25 +0000

    merged commonArtCard and ClevWorkingCard

--- curator-project/src/Components/common/CommonArtCard.jsx
+++ curator-project/src/Components/common/CommonArtCard.jsx
@@ -10,10 +10,13 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState, useEffect } from 'react'
 import { VAArtworkCollection, getOneVAArt } from '../../../api/api';
 import Grid from '@mui/material/Grid2';
+import Loading from './Loading';
+import { parsingClevData, parsingVAData } from '../../Utils/api_util';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -43,59 +46,88 @@ const ExpandMore = styled((props) => {
 const CommonArtCard = ({terms}) => {
 
     const [results, setResults] = useState();
+    const [error, setError] = useState();
     const [isLoading, setIsLoading] = useState(true);
+    const [toggledFavourites, isToggledFavourites] = useState(fetchStoredData);
+    function fetchStoredData() {
+      const storedData = localStorage.getItem("artworks");
+      return storedData ? JSON.parse(storedData) : [];
+    }
+    function handleFavourite(art) {
+      let updatedFavourites = [...toggledFavourites];
+      const found = updatedFavourites.some((item) => item.key === art.key);
+      if (!found) {
+        updatedFavourites = [...updatedFavourites, art];
+      } else {
+        updatedFavourites = updatedFavourites.filter(
+          (item) => item.key !== art.key
+        );
+      }
+      isToggledFavourites(updatedFavourites);
+      localStorage.setItem("artworks", JSON.stringify(updatedFavourites));
+    }
 
-    //testing with one artwork
+    //multiple artworks
     useEffect(() => {
     VAArtworkCollection(terms).then((res)=> {
-      const arr = []
-            setResults(res.records);
+      const parsedData = res.records.map((art)=> {return parsingVAData(art)})
+            setResults(parsedData);
+            setIsLoading(false);
+        }).catch((err)=>{
+            setError('Failed to load artwork data.');
             setIsLoading(false);
         });
     }, []);
-    isLoading===false?console.log(results):console.log('no')
-    isLoading===false?console.log(''):console.log('noimg')
 
     const [expanded, setExpanded] = useState(false);
 
     const handleExpandClick = () => {
       setExpanded(!expanded);
     };
+    const storedData = localStorage.getItem("artworks");
+
     return (
         <>
       {isLoading === false? (
-        <Grid container>
+        <Grid container spacing={2} sx={{ marginLeft: '150px' }}>
           {results.map((result) => {
-  console.log(result); // Keep your log if needed
 
-  return ( // Use 'return' here
-    <Card key={result.systemNumber} 
+  return ( 
+    <Card key={result.key} 
           sx={{ maxWidth: 345, m: 4, backgroundColor: '#ebe6e3' }}>
       <CardHeader
         avatar={
-          <Avatar alt="V&A" src="../../../assets/V&Asymbol.jpg" aria-label="museumIcon" />
+          <Avatar alt="V&A" src={result.avatar} aria-label="museumIcon" />
         }
-        title={result._primaryTitle}
-        subheader={`Made by: ${result._primaryMaker.name}`}
+        title={result.title}
+        subheader={`Made by: ${result.creator}`}
       />
       <CardMedia
         component="img"
         height="194"
-        image={`https://framemark.vam.ac.uk/collections/${result._primaryImageId}/full/600,400/0/default.jpg`}
-        alt={`No image of ${result._primaryTitle} was provided`}
+        image={result.image}
+        alt={`No image of '${result.title}' was provided`}
       />
       <CardContent>
         <Typography variant="body2" align='right' sx={{ color: 'text.secondary', margin: 1, fontWeight: 'bold' }}>
-          {`Dated: ${result.accessionNumber}`}
+          {`Dated: ${result.creationDate}`}
         </Typography>
-        <Typography sx={{ color: 'text.secondary' }}>
-          No description was provided for this piece.
-        </Typography>
+        <Typography sx={{ marginBottom: 2, color: 'text.secondary' }}>
+            {`Type: ${result.type}`}
+          </Typography>
+          <Typography sx={{ marginBottom: 2, color: 'text.secondary' }}>
+          {result.onDisplay}
+          </Typography>
+
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
+      <IconButton onClick={() => handleFavourite(results)} aria-label="add to favorites">
+            {toggledFavourites.some((item) => item.key === result.key) ? (
+                    <FavoriteIcon />
+                  ) : (
+                    <FavoriteBorderIcon />
+                  )}
+            </IconButton>
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
@@ -105,23 +137,22 @@ const CommonArtCard = ({terms}) => {
           <ExpandMoreIcon />
         </ExpandMore>
       </CardActions>
+
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography sx={{ marginBottom: 2, color: 'text.secondary' }}>
-            {`Type: ${result.objectType}`}
-          </Typography>
-          <Typography sx={{ marginBottom: 2, color: 'text.secondary' }}>
-            {result._currentLocation.onDisplay ? 'This piece is currently being displayed in the V&A' : 'This piece is in storage'}
-          </Typography>
+        <Typography sx={{ color: 'text.secondary' }}>
+          {result.description}
+        </Typography>
         </CardContent>
       </Collapse>
     </Card>
+    
   );
 })}
 
             </Grid>
         ):
-        <p>nothing</p>
+        <Loading/>
         }
         </>
       );
