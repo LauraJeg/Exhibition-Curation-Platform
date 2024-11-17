e5071a61a51a4d072f7ecbe89d4966d3f05d8377
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-11-17 22:02:59 +0000

    xhanginh base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Exhibition-Curation-Platform/',
+  base:'https://laurajeg.github.io/Exhibition-Curation-Platform/',
   plugins: [react()],
 })
